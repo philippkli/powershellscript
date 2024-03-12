6f674663c582030da103eb0f4763123a8b173ee6
philippkli <philipp.klingert@volkswagen-groupservices.com>
2024-03-12 12:31:44 +0100

    Final pre-review

--- oldfiles.ps1
+++ oldfiles.ps1
@@ -1,57 +1,87 @@
-#Ab dieser Anzahl an Tagen wird die Datei aufgerufen
-param (
-    [int]$days = 30
-)
-# Manuell den Pfad eingeben
-# $directory = ""
-# oder das Skript in den Ordner einfügen, alle Unterordner werden dabei auch in Betracht gezogen
-$directory = $PSScriptRoot
+# Beschreibung: Dieses Skript dient dazu, spezifizierte alte Dateien zu komprimieren und zu löschen.
+# Durch Parametern oder durch manuelle Eingabe kann entschieden werden, ab welchem "alter" Dateien berücksichtigt, ob sie komprimiert oder gelöscht werden. 
+# Datum: 12.03.2023 DD/MM/YYYY
+# Autor: Philipp Klingert (A-GSAA-W2 | philipp.klingert@volkswagen-groupservices.com)
+# Parameterliste: -days <int> -zip Y/N -del Y/N -log Y/N
 
-#Das heutige Datum erhalten
+param (
+    # Parameter gibt den Maximalwert in Tagen an. Alle Dateien die älter sind als der gegebene Wert werden betrachtet 
+    [int]$days = 0,
+    # Parameter ob die Dateien komprimiert werden sollen oder nicht
+    [string]$zip = "N",
+    # Parameter ob die Dateien nach der komprimierung gelöscht werden sollen
+    [string]$del = "N",
+    # Parameter ob das Skript in Powershell den Vorgang wiedergibt
+    [string]$log = "N"
+)
+# Der Ordner in dem sich das Script befindet
+$currentDirectory = $PSScriptRoot
+# Das heutige Datum erhalten
 $currentDate = Get-Date
 
-#Das "Deadline" Datum errechnen
-$cutoffDate = $currentDate.AddDays(-$days)
-
-#Datein finden, die älter sind als das Deadline Datum
-$oldFiles = Get-ChildItem -Path $directory | Where-Object { $_.LastWriteTime -lt $cutoffDate}
-
-#Schleife mit gekürztem Pfad im Output
-foreach ($file in $oldFiles) {
-    $relativePath = $file.FullName.Replace($directory, "").TrimStart("\")
-    Write-Host "Datei $relativePath ist aelter als $days tage"
-}
-
-# Nutzer frage, ob die alten Datein gezippt werden sollen
-$compressOldFiles = Read-Host "Moechten Sie die alten Dateien komprimieren? (Y/N)"
-
-if ($compressOldFiles -eq "Y" -or $compressOldFiles -eq "y") {
-    # Name/Zeit für .zip Datein erstellen
-    $timestamp = Get-Date -Format "yyyMMddHHmm"
-    
-    # Zip Datei erstellen
-    $zipFileName = "OldFiles_$timestamp.zip"
-    $zipPath = Join-Path -Path $directory -ChildPath $zipFileName
-    
-    # Datein komprimieren
-    Compress-Archive -Path $oldFiles.FullName -DestinationPath $zipPath
-    
-    Write-Host "Datein die aelter als $days Tage waren, wurden in $zipFileName komprimiert"
-} else {
-    "Alte Datein wurden nicht komprimiert"
-}
-
-# Nutzer fragen, ob die alten Datein gelöscht werden sollen
-$deleteOldFiles = Read-Host "Moechten Sie die alten unkomprimierten Dateien loeschen? (Y/N)"
-
-if ($deleteOldFiles -eq "Y" -or $deleteOldFiles -eq "y") {
+# Wird nur aufgerufen, wenn das Skript ohne jegliche Parameter aufgerufen wird - bspw. rechtsclick -> mit powershell ausführen
+# Fragt dementsprechend nach manuellen Eingaben und gibt automatisch den Vorgang wieder
+if ($days -eq 0 -and $zip -eq "N" -and $del -eq "N") {
+    $days = Read-Host "Geben Sie die Anzahl der Tage ein"
+    #Das manuell gesetzte "Deadline" Datum errechnen
+    $cutoffDate = $currentDate.AddDays(-$days)
+    #Datein finden, die älter sind als das anuell gesetzte Deadline Datum
+    $oldFiles = Get-ChildItem -Path $currentDirectory | Where-Object { $_.LastWriteTime -lt $cutoffDate}
+    #Zeigt dem User an, welche Dateien älter sind als $days
     foreach ($file in $oldFiles) {
-        #Remove-Item -Path $file.FullName -Force
-        Write-Host "$($file.FullName) wurde 'geloescht'"
+        $relativePath = $file.FullName.Replace($currentDirectory, "").TrimStart("\")
+        Write-Host "Datei $relativePath ist aelter als $days tage"
     }
-    Write-Host "Alte Datein wurden geloescht"
-} else {
-    Write-Host "Alte Datein wurden nicht geloescht"
+    # Nutzer fragen, ob die alten Datein komprimiert werden sollen
+    $zip = Read-Host "Moechten Sie die alten Dateien komprimieren? (Y/N)"
+    # Nutzer fragen, ob die alten Datein gelöscht werden sollen
+    $del = Read-Host "Moechten Sie die alten Dateien loeschen? (Y/N)"
+    $log = "Y"
 }
 
-Pause
+# Das "Deadline" Datum errechnen
+$cutoffDate = $currentDate.AddDays(-$days)
+# Datein finden, die älter sind als das Deadline Datum
+$oldFiles = Get-ChildItem -Path $currentDirectory | Where-Object { $_.LastWriteTime -lt $cutoffDate}
+
+# Wenn die Dateien komprimiert werden sollen, dann wird der folgende Code ausgeführt
+if ($zip -eq "Y" -or $zip -eq "y") {
+    foreach ($file in $oldFiles) {
+        # Name/Zeit für .zip Datein erstellen
+        $timestamp = Get-Date -Format "yyyMMddHHmm"
+        # Zip Datei erstellen
+        $zipFileName = "old_$($file.BaseName)_$timestamp.zip"
+        $zipPath = Join-Path -Path $currentDirectory -ChildPath $zipFileName
+        # Datein komprimieren
+        Compress-Archive -Path $file.FullName -DestinationPath $zipPath
+        if ($log -eq "Y" -or $log -eq "y") {
+            Write-Host "Datei $($file.FullName) wurde in $zipFileName komprimiert" #test
+        }
+    }
+} else {
+    if ($log -eq "Y" -or $log -eq "y") {
+        Write-Host "Dateien wurden nicht komprimiert" #test
+    }
+}
+
+# Wenn die alten unkomprimierten Dateien gelöscht werden sollen, dann wird der folgende Code ausgeführt
+if ($del -eq "Y" -or $del -eq "y") {
+    foreach ($file in $oldFiles) {
+        # Löscht die alten unkomprimierten Dateien. VORSICHT!!!!!
+        Remove-Item -Path $file.FullName -Force
+        if ($log -eq "Y" -or $log -eq "y") {
+        Write-Host "$($file.FullName) wurde 'geloescht'" #test
+        }
+    }
+    if ($log -eq "Y" -or $log -eq "y") {
+    Write-Host "Alte Dateien wurden erfolgreich geloescht" #test
+    }
+} else {
+    if ($log -eq "Y" -or $log -eq "y") {
+    Write-Host "Alte Dateien wurden nicht geloescht" #test
+    }
+}
+
+if ($log -eq "Y" -or $log -eq "y") {
+    Pause
+}
